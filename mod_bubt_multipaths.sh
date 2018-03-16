8ba9cf6235758d917bd9ba8b6a6b44312979bf0c
prataprc <prataprc@gmail.com>
2018-03-16 18:04:11 +0530

    two scripts for bubt, one with single disk shard and many.

--- mod_bubt_multipaths.sh
+++ mod_bubt_multipaths.sh
@@ -1,7 +1,7 @@
 rm dbtest
 go build
 
-ARGS="-db bubt -key 22 -value 128"
+ARGS="-db bubt -key 22 -value 128 -npaths 3"
 LOADS="-load 10000000"
 READS="-reads 60000000"
 echo "./dbtest $ARGS $LOADS $READS"
