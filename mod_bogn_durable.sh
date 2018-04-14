bd0cc8e086e5ba7c98af6f3494f98e001887b00a
prataprc <prataprc@gmail.com>
2018-04-14 17:43:05 +0530

    increase ops for mod_bogn_durable.sh

--- mod_bogn_durable.sh
+++ mod_bogn_durable.sh
@@ -4,14 +4,14 @@ rm dbtest; go build
 
 echo -e "#########################################\n"
 ARGS="-db bogn -bogn durable -lsm -period 25 -key 32 -value 1024"
-OPS="-load 100000 -writes 400000"
+OPS="-load 1000000 -writes 4000000"
 echo "./dbtest $ARGS $OPS"
 ./dbtest $ARGS $OPS
 echo
 
 echo -e "#########################################\n"
 ARGS="-db bogn -bogn durable -lsm -period 25 -key 32 -value 1024 -randwidth"
-OPS="-load 100000 -writes 400000"
+OPS="-load 1000000 -writes 4000000"
 echo "./dbtest $ARGS $OPS"
 ./dbtest $ARGS $OPS
 echo
